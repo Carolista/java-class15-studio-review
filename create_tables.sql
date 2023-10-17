b00e4230fa33c7ba163afb26f0cc2c5d2be321ab
Caroline R. Jones <55961845+Carolista@users.noreply.github.com>
2023-10-17 14:20:59 -0500

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,6 @@
--- Create book table & import records from books.csv
+/* CREATE TABLES & IMPORT DATA FROM CSV FILES */
+
+-- Create book table
 CREATE TABLE book (
    book_id INT AUTO_INCREMENT PRIMARY KEY,
    author_id INT,
@@ -8,7 +10,9 @@ CREATE TABLE book (
    genre_id INT
 );
 
--- Create author table & import records from authors.csv
+-- Before executing the next query, import records from books.csv to the new book table
+
+-- Create author table
 CREATE TABLE author (
    author_id INT AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR(255),
@@ -17,13 +21,17 @@ CREATE TABLE author (
    deathday DATE
 );
 
--- Create patron table & import records from patrons.csv
+-- Before executing the next query, import records from authors.csv to the new author table
+
+-- Create patron table
 CREATE TABLE patron (
    patron_id INT AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR(255),
    last_name VARCHAR(255),
    loan_id INT
 );
+
+-- Before executing the next query, import records from patrons.csv to the new patron table
 
 -- Create reference_books table
 CREATE TABLE reference_books (
@@ -40,11 +48,13 @@ CREATE TABLE reference_books (
 INSERT INTO reference_books(edition, book_id)
 VALUE (5,32);
 
--- Create genre table & import records from genres.csv
+-- Create genre table
 CREATE TABLE genre (
    genre_id INT PRIMARY KEY,
    genres VARCHAR(100)
 );
+
+-- Before executing the next query, import records from genres.csv to the new genre table
 
 -- Create loan table 
 CREATE TABLE loan (
@@ -58,3 +68,5 @@ CREATE TABLE loan (
       ON UPDATE SET NULL
       ON DELETE SET NULL
 );
+
+-- Now you're ready to write queries (see write_queries.sql)
